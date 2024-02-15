12fbf15adde8f567288bbf3424fcb8c6bde1bf0e
nathan Ventura <nathanventura.dev@gmail.com>
2024-02-14 22:09:09 -0300

    Creating the start method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Start();
+      }
+      static void Start()
+      {
+         int time = 10;
+         int currentTime = 0;
+         while (currentTime != time)
+         {
+            Console.Clear();
+            currentTime++;
+            Console.WriteLine(currentTime);
+            Thread.Sleep(1000);
+         }
+      }
+   }
+}
